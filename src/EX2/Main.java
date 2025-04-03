8ce6dafb88e4329df19a22e99ae17dc83fcc5b7d
Bruno Müller Crespo <bruno.crespo@al.infnet.edu.br>
2025-04-03 08:21:36 -0300

    Exercício 2

---
+++ src/EX2/Main.java
@@ -0,0 +1,30 @@
+package EX2;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite a sua senha: ");
+
+        String senha = leitor.next();
+
+        if (senha.length() < 8){
+            System.out.println("ERRO: senha deve conter pelo menos 8 caracteres");
+        } else{
+            int quantidadeEspeciais = 0;
+            for(int i = 0; i < senha.length(); i++){
+                char c = senha.charAt(i);
+
+                if (!Character.isDigit(c) && !Character.isLetter(c) && !Character.isWhitespace(c)){
+                    quantidadeEspeciais++;
+                }
+            }
+
+            if(quantidadeEspeciais > 0){
+                System.out.println("Senha cadastrada com sucesso");
+            } else{
+                System.out.println("ERRO: senha deve conter pelo menos 1 caracter especial");
+            }
+        }
+    }
+}
