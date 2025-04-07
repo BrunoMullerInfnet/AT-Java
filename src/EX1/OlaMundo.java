788bb76bc19ba972055294256a874fce590cc71c
bruno.crespo <bruno.crespo@al.infnet.edu.br>
2025-04-07 07:08:13 -0300

    Finalizando AT

--- src/EX1/OlaMundo.java
+++ src/EX1/OlaMundo.java
@@ -3,6 +3,6 @@ package EX1;
 public class OlaMundo {
     public static void main(String[] args){
         String nome = "Bruno";
-        System.out.printf("Olá, meu nome é %s e estou aprendendo Java",nome);
+        System.out.printf("Olá, meu nome é %s e estou aprendendo Java", nome);
     }
 }
