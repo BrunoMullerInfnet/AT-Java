34e8df0ec353b513cd1f430f7e87ee8db258412b
bruno.crespo <bruno.crespo@al.infnet.edu.br>
2025-04-07 08:59:39 -0300

    Finalizando AT

--- src/EX2/Main.java
+++ src/EX2/Main.java
@@ -8,23 +8,33 @@ public class Main {
 
         String senha = leitor.next();
 
+
         if (senha.length() < 8){
             System.out.println("ERRO: senha deve conter pelo menos 8 caracteres");
         } else{
             int quantidadeEspeciais = 0;
+            int quantidadeNumeros = 0;
             for(int i = 0; i < senha.length(); i++){
                 char c = senha.charAt(i);
 
                 if (!Character.isDigit(c) && !Character.isLetter(c) && !Character.isWhitespace(c)){
                     quantidadeEspeciais++;
                 }
+                if (Character.isDigit(c)){
+                    quantidadeNumeros++;
+                }
             }
 
             if(quantidadeEspeciais > 0){
-                System.out.println("Senha cadastrada com sucesso");
+                if(quantidadeNumeros > 0){
+                    System.out.println("Senha cadastrada com sucesso");
+                } else{
+                    System.out.println("ERRO: senha deve conter pelo menos 1 número");
+                }
             } else{
                 System.out.println("ERRO: senha deve conter pelo menos 1 caracter especial");
             }
+
         }
     }
 }
